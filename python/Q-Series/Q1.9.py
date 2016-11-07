ba1ddd20ef762db034247079727d81482578eb10
Luckylau <laujunbupt0203@163.com>
2016-11-07 09:20:57 +0800

    Q1.8,Q1.9 pep8 criterion

--- python/Q-Series/Q1.9.py
+++ python/Q-Series/Q1.9.py
@@ -7,17 +7,21 @@ Email:laujunbupt0913@163.com
 '''
 import unittest
 
-def isSubstring(str1,str2):
-    if len(str1)==len(str2)!=0:
-        if (str1+str1).find(str2)!=-1:
+
+def isSubstring(str1, str2):
+    if len(str1) == len(str2) != 0:
+        if (str1 + str1).find(str2) != -1:
             return True
     else:
         return False
+
+
 class Test(unittest.TestCase):
-    data=[('ilovepython','ovepythonil'),('ihaveapple','appleihave')]
+    data = [('ilovepython', 'ovepythonil'), ('ihaveapple', 'appleihave')]
+
     def test(self):
         for test_str1, test_str2 in self.data:
-            res=isSubstring(test_str1,test_str2)
+            res = isSubstring(test_str1, test_str2)
             self.assertTrue(res)
 
 if __name__ == '__main__':
