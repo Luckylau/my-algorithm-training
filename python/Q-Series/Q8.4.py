0e23f09a070437940205cbbf690db6ef64120be3
Luckylau <laujunbupt0203@163.com>
2017-04-06 13:53:28 +0800

    Q8.4 update

--- python/Q-Series/Q8.4.py
+++ python/Q-Series/Q8.4.py
@@ -23,10 +23,31 @@ def getSubsets(setz, index):
 
     return allSubsets
 
+
 def getSubsets2(setz):
-    pass
+
+    allSubsets = []
+    max = 1 << len(setz)
+    for k in range(max):
+        subset = convertIntforGet(setz, k)
+        allSubsets.append(subset)
+    return allSubsets
+
+
+def convertIntforGet(setz, k):
+
+    subset = []
+    index = 0
+    i = k
+    while i > 0:
+        if i & 1 == 1:
+            subset.append(setz[index])
+        index += 1
+        i >>= 1
+    return subset
 
 
 if __name__ == '__main__':
     set = ([1, 2, 3])
     print getSubsets(set, 0)
+    print getSubsets2(set)
