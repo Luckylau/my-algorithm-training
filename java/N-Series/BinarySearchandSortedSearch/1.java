000981b199f7b25169f444a0535b4e05763d602f
Luckylau <laujunbupt0203@163.com>
2017-04-15 22:32:07 +0800

    classic binary search

---
+++ java/N-Series/BinarySearchandSortedSearch/1.java
@@ -0,0 +1,8 @@
+/**
+ * Classical Binary Search
+ *
+ * Given an sorted integer array - nums, and an integer - target. Find the first position of
+ * target in nums, return -1 if target doesn¡¯t exist.
+ *
+ */
+ 
