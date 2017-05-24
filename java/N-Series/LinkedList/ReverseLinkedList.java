9acad3d82485ea315fb35566affa3093e69ad846
Luckylau <laujunbupt0203@163.com>
2017-05-24 14:32:54 +0800

    Reverse Linked List

---
+++ java/N-Series/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+/**
+ * Reverse a linked list.
+ * Example
+ * For linked list 1->2->3, the reversed linked list is 3->2->1
+ * Challenge 
+ * Reverse it in-place and in one-pass
+ * 
+ * 
+ */
+package chapter4;
+
+public class ReverseLinkedList {
+	
+	 public ListNode reverse(ListNode head) {
+		 if ( head == null) {
+			 return null;
+		 }
+		 
+		 ListNode pre = null;
+		 while (head != null ){
+			 ListNode temp = head.next;
+			 head.next = pre;
+			 pre = head;
+			 head = temp;
+		 }
+		 return pre;
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		ListNode node = new ListNode(2);
+		ListNode node2 = new ListNode(3);
+		ListNode node3 = new ListNode(4);
+		ListNode node4 = new ListNode(5);
+		head.next = node;
+		node.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		System.out.println(ListNode.printListfromHeadtoTail(head));
+		ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+		System.out.println(ListNode.printListfromHeadtoTail(reverseLinkedList.reverse(head)));
+
+	}
+
+}
