4ca48922262e7f23797da3224c0123793c737b3d
Luckylau <laujunbupt0203@163.com>
2017-08-22 14:45:30 +0800

    update RecoverSortedArray

--- java/N-Series/BinarySearchandSortedSearch/RecoverSortedArray.java
+++ java/N-Series/BinarySearchandSortedSearch/RecoverSortedArray.java
@@ -12,31 +12,51 @@
 package chapter2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RecoverSortedArray {
-	
+
 	public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
-        // write your code
-        for ( int index = 0 ; index < nums.size() -1 ; index ++ ) {
-            if ( nums.get(index) > nums.get(index + 1) ) {
-                reverse( nums,0,index);
-                reverse( nums,index + 1,nums.size()-1);
-                reverse( nums,0,nums.size()-1);
-                return ;
-            }
-        }
-    }
-    private void reverse(ArrayList<Integer> nums , int start ,int end) {
-        for ( int i = start , j = end ; i < j ; i++ , j-- ) {
-            int tmp = nums.get( i );
-            nums.set( i,nums.get(j));
-            nums.set( j, tmp);
-        }
-    }
+		// write your code
+		for (int index = 0; index < nums.size() - 1; index++) {
+			if (nums.get(index) > nums.get(index + 1)) {
+				reverse(nums, 0, index);
+				reverse(nums, index + 1, nums.size() - 1);
+				reverse(nums, 0, nums.size() - 1);
+				return;
+			}
+		}
+	}
+
+	private void reverse(ArrayList<Integer> nums, int start, int end) {
+		for (int i = start, j = end; i < j; i++, j--) {
+			int tmp = nums.get(i);
+			nums.set(i, nums.get(j));
+			nums.set(j, tmp);
+		}
+	}
+
+	public void recoverRotatedSortedArray(int[] nums) {
+		for (int index = 0; index < nums.length - 1; index++) {
+			if (nums[index] > nums[index + 1]) {
+				reverse(nums, 0, index);
+				reverse(nums, index + 1, nums.length - 1);
+				reverse(nums, 0, nums.length - 1);
+			}
+		}
+	}
+
+	private void reverse(int[] nums, int start, int end) {
+		for (int i = start, j = end; i < j; i++, j--) {
+			int tmp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = tmp;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> nums =new ArrayList<Integer>();
+		ArrayList<Integer> nums = new ArrayList<Integer>();
 		nums.add(4);
 		nums.add(5);
 		nums.add(1);
@@ -46,8 +66,11 @@ public class RecoverSortedArray {
 		RecoverSortedArray recoverSortedArray = new RecoverSortedArray();
 		recoverSortedArray.recoverRotatedSortedArray(nums);
 		System.out.println(nums.toString());
-		
- 
+		int[] numbers = new int[] { 4, 5, 1, 2, 3 };
+		System.out.println("numbers init : " + Arrays.toString(numbers));
+		recoverSortedArray.recoverRotatedSortedArray(numbers);
+		System.out.println("numbers : " +  Arrays.toString(numbers));
+
 	}
 
 }
