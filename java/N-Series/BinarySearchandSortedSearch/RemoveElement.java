c23275a7e95e54f0db702d7e82a1359eac7f7520
Luckylau <laujunbupt0203@163.com>
2017-08-22 16:06:00 +0800

    update RemoveElement

--- java/N-Series/BinarySearchandSortedSearch/RemoveElement.java
+++ java/N-Series/BinarySearchandSortedSearch/RemoveElement.java
@@ -7,31 +7,37 @@
  */
 package chapter2;
 
+import java.util.Arrays;
 
 public class RemoveElement {
-	
-	 public int removeElement(int[] A, int elem) {
-	        // write your code here
-	        int i = 0;
-	        int pointer = A.length - 1;
-	        while (i <= pointer) {
-	            if (A[i] == elem) {
-	                A[i] = A[pointer];
-	                pointer--;
-	            } else {
-	                i++;
-	            }
-	        }
-	        return pointer + 1;
-	    }
+
+	public int removeElement(int[] A, int elem) {
+		// write your code here
+		int i = 0;
+		int pointer = A.length - 1;
+		while (i <= pointer) {
+			if (A[i] == elem) {
+				A[i] = A[pointer];
+				pointer--;
+			} else {
+				i++;
+			}
+		}
+		return pointer + 1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int []nums =new int []{0,4,4,0,0,2,4,4};
-		System.out.println("the length of old array is "+nums.length);
+		int[] nums = new int[] { 0, 4, 4, 0, 0, 2, 4, 4 };
+		System.out.println(Arrays.toString(nums));
+		System.out.println("the length of old array is " + nums.length);
 		RemoveElement removeElement = new RemoveElement();
-		System.out.println("the length of new array is "+removeElement.removeElement(nums, 4));
-	
+		int len = removeElement.removeElement(nums, 4);
+		System.out.println("the length of new array is " + len);
+		System.out.println(Arrays.toString(nums));
+		nums = Arrays.copyOf(nums, len);
+		System.out.println(Arrays.toString(nums));
+
 	}
 
 }
