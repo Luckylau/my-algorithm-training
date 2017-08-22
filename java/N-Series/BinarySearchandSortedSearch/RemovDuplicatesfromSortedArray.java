9a7337a3e793257dd01d3587725de0c372d13b3d
Luckylau <laujunbupt0203@163.com>
2017-08-22 14:59:45 +0800

    update RemoveDuplicatesfromSortedArray

--- java/N-Series/BinarySearchandSortedSearch/RemovDuplicatesfromSortedArray.java
+++ java/N-Series/BinarySearchandSortedSearch/RemovDuplicatesfromSortedArray.java
@@ -8,29 +8,34 @@
  */
 package chapter2;
 
+import java.util.Arrays;
 
 public class RemovDuplicatesfromSortedArray {
-	
-	 public int removeDuplicates(int[] nums) {
-	        // write your code here
-	        if ( nums.length == 0 ){
-	            return 0;
-	        }
-	        int j = 0;
-	        for(int i =0 ;i < nums.length; i++ ) {
-	            if (nums[i] != nums[j]) {
-	                j++;
-	                nums[j] = nums[i];
-	            }
-	        }
-	        return j + 1;
-	    }
+
+	public int removeDuplicates(int[] nums) {
+		// write your code here
+		if (nums.length == 0) {
+			return 0;
+		}
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != nums[j]) {
+				j++;
+				nums[j] = nums[i];
+			}
+		}
+		return j + 1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] nums = new int [] {1,1,2,4,4,5,5,5,8,9,12,15,16};
+		int[] nums = new int[] { 1, 1, 2, 4, 4, 5, 5, 5, 8, 9, 12, 15, 16 };
+		System.out.println(Arrays.toString(nums));
 		RemovDuplicatesfromSortedArray removDuplicatesfromSortedArray = new RemovDuplicatesfromSortedArray();
-		System.out.println("the length is " + removDuplicatesfromSortedArray.removeDuplicates(nums));
+		int length = removDuplicatesfromSortedArray.removeDuplicates(nums);
+		System.out.println("the length is " + length);
+		nums = Arrays.copyOf(nums, length);
+		System.out.println(Arrays.toString(nums));
 
 	}
 
