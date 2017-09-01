13f963067629728fd09439bb67460d2e84c6baeb
Luckylau <laujunbupt0203@163.com>
2017-09-01 17:36:38 +0800

    update

--- java/N-Series/strStr/Permutations.java
+++ java/N-Series/strStr/Permutations.java
@@ -20,31 +20,35 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
-	
+
 	public List<List<Integer>> permute(int[] nums) {
-        // write your code here
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> sub = new ArrayList<Integer>();
-        Helper(sub ,nums , result);
-        return result;
-    }
-    private void Helper(List<Integer> sub , int[]nums ,List<List<Integer>> result) {
-        if (sub.size() == nums.length) {
-            result.add(new ArrayList<Integer>(sub));
-        }
-        for (int i = 0 ; i < nums.length; i++) {
-            if(sub.contains(nums[i])){
-                continue;
-            }
-            sub.add(nums[i]);
-            Helper(sub, nums , result);
-            sub.remove(sub.size() - 1);
-        }
-    }
+		// write your code here
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> sub = new ArrayList<Integer>();
+		boolean[] visited = new boolean[nums.length];
+		Helper(sub, visited, nums, result);
+		return result;
+	}
+
+	private void Helper(List<Integer> sub, boolean[] visited, int[] nums, List<List<Integer>> result) {
+		if (sub.size() == nums.length) {
+			result.add(new ArrayList<Integer>(sub));
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (visited[i] == true) {
+				continue;
+			}
+			visited[i] = true;
+			sub.add(nums[i]);
+			Helper(sub, visited, nums, result);
+			sub.remove(sub.size() - 1);
+			visited[i] = false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = new int[] { 4, 1, 0 };
+		int[] nums = new int[] { 1, 2, 0 };
 		Permutations Permutations = new Permutations();
 		System.out.println(Arrays.deepToString(Permutations.permute(nums).toArray()));
 
