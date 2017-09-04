a4614f3a6c5b534544ea1401040d28a54ef37ba1
Luckylau <laujunbupt0203@163.com>
2017-09-04 14:21:59 +0800

    MaxProduct

---
+++ java/C-Series/EvenoddOrder.java
@@ -0,0 +1,36 @@
+package demo;
+
+import java.util.Arrays;
+
+public class EvenoddOrder {
+
+	public void evenoddSort(int[] nums) {
+		if (nums.length == 0 || nums == null) {
+			return;
+		}
+		int start = 0;
+		int end = nums.length - 1;
+		while (start < end) {
+			if (nums[start] % 2 == 0) {
+				while (nums[end] % 2 == 0) {
+					end--;
+				}
+				int tmp = nums[start];
+				nums[start] = nums[end];
+				nums[end] = tmp;
+				end--;
+			}
+			start++;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = new int[] { 3, 4, 1, 5, 7, 2, 8, 10, 8, 7 };
+		EvenoddOrder evenoddSort = new EvenoddOrder();
+		evenoddSort.evenoddSort(nums);
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+}
