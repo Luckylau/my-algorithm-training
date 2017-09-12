4fda928351bd29a119118e7355c07fe85ff4973e
Luckylau <laujunbupt0203@163.com>
2017-09-12 21:35:07 +0800

    findlongDigtal

---
+++ java/C-Series/DistanceBTree.java
@@ -0,0 +1,42 @@
+package demo;
+
+public class DistanceBTree {
+	int max = 0;
+
+	public int distanceBTree(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		helper(root);
+		return max;
+	}
+
+	private int helper(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = helper(root.left);
+		int right = helper(root.right);
+		int tmp = left + right + 1;
+		if (max < tmp)
+			max = tmp;
+		return Math.max(left, right) + 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(1);
+		TreeNode node1 = new TreeNode(2);
+		TreeNode node2 = new TreeNode(3);
+		TreeNode node3 = new TreeNode(4);
+		TreeNode node4 = new TreeNode(5);
+		root.left = node1;
+		root.right = node2;
+		node1.left = node3;
+		node1.right = node4;
+		DistanceBTree DistanceBTree = new DistanceBTree();
+		System.out.println(DistanceBTree.distanceBTree(root));
+
+	}
+
+}
---
+++ java/C-Series/FindlongDigtal.java
@@ -0,0 +1,44 @@
+package demo;
+/**
+ * 
+ * 在字符串中找出连续最长的数字串，并把这个串的长度返回。
+ *
+ */
+public class FindlongDigtal {
+
+	public String findlongDigtal(String input) {
+		if (input.length() == 0 || input == null) {
+			return null;
+		}
+		int maxLen = 0;
+		int index = -1;
+		int len = 0;
+		int start = 0;
+		while (start < input.length()) {
+			if (input.charAt(start) <= '9' && input.charAt(start) >= '0') {
+				len++;
+			} else {
+				if (len > maxLen) {
+					maxLen = len;
+					index = start - len;
+				}
+				len = 0;
+			}
+			start++;
+		}
+		if (len > maxLen) {
+			maxLen = len;
+			index = start - len;
+		}
+		return input.substring(index, index + maxLen);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FindlongDigtal findlongDigtal = new FindlongDigtal();
+		String str = "abcd12Y3fsfsa45ed125ss14151351123456789&&&&&&";
+		System.out.println(findlongDigtal.findlongDigtal(str));
+
+	}
+
+}
