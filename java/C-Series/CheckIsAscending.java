808304bcac5fe7330b5dddfcd41ccbad013f8ccd
Luckylau <laujunbupt0203@163.com>
2017-09-18 19:17:41 +0800

    CheckIsAscending

---
+++ java/C-Series/CheckIsAscending.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class CheckIsAscending {
+	public boolean checkIsAscending(int[] nums) {
+		if (nums.length == 0 || nums == null) {
+			return false;
+		}
+		return helper(nums, nums.length - 1);
+	}
+
+	private boolean helper(int[] nums, int end) {
+		if (end == 0) {
+			return true;
+		}
+		if (nums[end] < nums[end - 1]) {
+			return false;
+		}
+		return helper(nums, end - 1);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+		CheckIsAscending CheckIsAscending = new CheckIsAscending();
+		System.out.println(CheckIsAscending.checkIsAscending(nums));
+	}
+
+}
