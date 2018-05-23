0706b07407ef66d0be5594d025c68bb679b5448a
luckylau <laujunbupt0203@163.com>
2018-05-23 15:29:07 +0800

    合并2个有序数组

--- src/n/series/BinarySearchandSortedSearch/MergeSortedArray.java
+++ src/n/series/BinarySearchandSortedSearch/MergeSortedArray.java
@@ -1,26 +1,32 @@
-/**
- * Given two sorted integer arrays A and B, merge B into A as one sorted array.
- * Notice
- * You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. 
- * The number of elements initialized in A and B are m and n respectively.
- * Example
- * A = [1, 2, 3, empty, empty], B = [4, 5]
- * After merge, A will be filled as [1, 2, 3, 4, 5]
- * 
- */
 package n.series.BinarySearchandSortedSearch;
 
 import java.util.Arrays;
 
+/**
+ * Given two sorted integer arrays A and B, merge B into A as one sorted array.
+ * Notice
+ * You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B.
+ * The number of elements initialized in A and B are m and n respectively.
+ * Example
+ * A = [1, 2, 3, empty, empty], B = [4, 5]
+ * After merge, A will be filled as [1, 2, 3, 4, 5]
+ */
 public class MergeSortedArray {
-	
-	public void mergeSortedArray(int[] A, int m, int[] B, int n) {
-        // write your code here
+
+    public static void main(String[] args) {
+        int[] A = new int[]{9, 10, 11, 12, 13, 0, 0, 0, 0};
+        int[] B = new int[]{4, 5, 6, 7};
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        mergeSortedArray.mergeSortedArray(A, 5, B, 4);
+        System.out.println(Arrays.toString(A));
+    }
+
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
         int index = m + n;
         while (m > 0 && n > 0) {
-            if (A[m - 1] > B[n -1]) {
+            if (A[m - 1] > B[n - 1]) {
                 A[--index] = A[--m];
-            }else {
+            } else {
                 A[--index] = B[--n];
             }
         }
@@ -28,13 +34,5 @@ public class MergeSortedArray {
             A[--index] = B[--n];
         }
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] A=new int [] {9,10,11,12,13,0,0,0,0};
-		int [] B=new int [] {4,5,6,7};
-		MergeSortedArray mergeSortedArray = new MergeSortedArray();
-		mergeSortedArray.mergeSortedArray(A, 5, B, 4);
-		System.out.println(Arrays.toString(A));
-	}
 
 }
