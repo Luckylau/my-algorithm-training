31b1df0b8e1eac0e07a107a65a64b305bfb194b6
luckylau <laujunbupt0203@163.com>
2018-05-23 14:16:25 +0800

    查找峰值

--- src/n/series/BinarySearchandSortedSearch/FindaPeak.java
+++ src/n/series/BinarySearchandSortedSearch/FindaPeak.java
@@ -1,4 +1,5 @@
 /**
+ * 查找峰值
  * There is an integer array which has the following features:
  * The numbers in adjacent positions are different.
  * A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
@@ -12,35 +13,33 @@
 package n.series.BinarySearchandSortedSearch;
 
 public class FindaPeak {
-	
-	public int findPeak(int[] A) {
-        // write your code here
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 2, 4, 5, 6, 7, 9, 8, 10, 12, 11};
+        FindaPeak findaPeak = new FindaPeak();
+        System.out.println(findaPeak.findPeak(nums));
+
+    }
+
+    public int findPeak(int[] A) {
         int start = 0;
         int end = A.length - 1;
-        while (start + 1 < end){
+        while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]){
+            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
                 return mid;
             }
-            if (A[mid] > A[start] && A[mid] < A[mid + 1]){
+            if (A[mid] > A[start] && A[mid] < A[mid + 1]) {
                 start = mid;
             } else {
                 end = mid;
             }
         }
-        if (A[start + 1] < A[start] && A[start] > A[start - 1]){
+        if (A[start + 1] < A[start] && A[start] > A[start - 1]) {
             return start;
         } else {
             return end;
         }
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[]nums=new int[]{1,3,2,4,5,6,7,9,8,10,12,11};
-		FindaPeak findaPeak = new FindaPeak();
-		System.out.println(findaPeak.findPeak(nums));
-
-	}
 
 }
