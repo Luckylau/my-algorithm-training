2c4257696b0513730114cc7cadde500e4d6288df
luckylau <laujunbupt0203@163.com>
2018-05-24 14:44:11 +0800

    翻转字符串

--- src/n/series/BinarySearchandSortedSearch/RotateString.java
+++ src/n/series/BinarySearchandSortedSearch/RotateString.java
@@ -1,3 +1,7 @@
+package n.series.BinarySearchandSortedSearch;
+
+import java.util.Arrays;
+
 /**
  * Given a string and an offset, rotate string by offset. (rotate from left to right)
  * Example
@@ -6,31 +10,31 @@
  * offset=1 => "gabcdef"
  * offset=2 => "fgabcde"
  * offset=3 => "efgabcd"
- * Challenge 
+ * Challenge
  * Rotate in-place with O(1) extra memory.
- * 
- * 
  */
-package n.series.BinarySearchandSortedSearch;
-
-import java.util.Arrays;
-
 public class RotateString {
-	
-	public void rotateString(char[] str, int offset) {
-        // write your code here
-        if ( str == null || str.length == 0) {
-            return ;
+
+    public static void main(String[] args) {
+        char[] str = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+        RotateString rotateString = new RotateString();
+        rotateString.rotateString(str, 2);
+        System.out.println(Arrays.toString(str));
+    }
+
+    public void rotateString(char[] str, int offset) {
+        if (str == null || str.length == 0) {
+            return;
         }
         int len = str.length;
         offset %= len;
-        reverse ( str , 0, len - offset -1);
-        reverse ( str , len - offset, len -1);
-        reverse ( str , 0, len -1);
+        reverse(str, 0, len - offset - 1);
+        reverse(str, len - offset, len - 1);
+        reverse(str, 0, len - 1);
     }
-    
-    private void reverse(char[] str ,int start ,int end ) {
-        while ( start < end ) {
+
+    private void reverse(char[] str, int start, int end) {
+        while (start < end) {
             char tmp = str[start];
             str[start] = str[end];
             str[end] = tmp;
@@ -38,15 +42,5 @@ public class RotateString {
             end--;
         }
     }
-		
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		char [] str = new char [] {'a','b','c','d','e','f','g'};
-		RotateString rotateString = new RotateString();
-		rotateString.rotateString(str, 2);
-		System.out.println(Arrays.toString(str));
-	}
 
 }
