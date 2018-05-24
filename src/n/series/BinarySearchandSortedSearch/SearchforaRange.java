22a65c75628b5490282f6ebc27be47a173a3167b
luckylau <laujunbupt0203@163.com>
2018-05-24 17:44:37 +0800

    二分查找变种

--- src/n/series/BinarySearchandSortedSearch/SearchforaRange.java
+++ src/n/series/BinarySearchandSortedSearch/SearchforaRange.java
@@ -1,67 +1,65 @@
-/**
- * Search for a Range
- * Given a sorted array of n integers, find the starting and ending position of a given target value.
- * If the target is not found in the array, return [-1, -1].
- * For example, 
- * Given [5, 7, 7, 8, 8, 10] and target value 8, 
- * return [3, 4].
- */
 package n.series.BinarySearchandSortedSearch;
 
 import java.util.Arrays;
 
+/**
+ * Search for a Range
+ * Given a sorted array of n integers, find the starting and ending position of a given target value.
+ * If the target is not found in the array, return [-1, -1].
+ * For example,
+ * Given [5, 7, 7, 8, 8, 10] and target value 8,
+ * return [3, 4].
+ */
 public class SearchforaRange {
-	
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 2, 4, 4};
+        SearchforaRange searchforaRange = new SearchforaRange();
+        System.out.println(Arrays.toString(searchforaRange.searchRange(nums, 2)));
+    }
+
     public int[] searchRange(int[] A, int target) {
-        // write your code here
-        if (A.length == 0){
-            return new int[]{-1,-1};
+        if (A == null || A.length == 0) {
+            return new int[]{-1, -1};
         }
         int start = 0;
-        int end = A.length-1;
-        int [] bound = new int [2];
-        while(start + 1 < end){
-            int mid = start + (end - start)/2;
-            if(A[mid] >= target){
+        int end = A.length - 1;
+        int[] bound = new int[2];
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] >= target) {
                 end = mid;
-            }else{
+            } else {
                 start = mid;
             }
         }
-        if(A[start] == target){
+        if (A[start] == target) {
             bound[0] = start;
-        }else if(A[end] == target){
+        } else if (A[end] == target) {
             bound[0] = end;
-        }else{
+        } else {
             bound[0] = bound[1] = -1;
             return bound;
         }
+
         start = 0;
-        end = A.length-1;
-        while(start + 1 < end){
-            int mid = start + (end - start)/2;
-            if(A[mid] <= target){
+        end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] <= target) {
                 start = mid;
-            }else{
+            } else {
                 end = mid;
             }
         }
-        if(A[end] == target){
+        if (A[end] == target) {
             bound[1] = end;
-        }else if (A[start] == target){
+        } else if (A[start] == target) {
             bound[1] = start;
-        }else {
-            bound[0]=bound[1] = -1;
+        } else {
+            bound[0] = bound[1] = -1;
         }
         return bound;
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] nums = new int []{1,2,2,3,4};
-		SearchforaRange searchforaRange=new SearchforaRange();
-		System.out.println(Arrays.toString(searchforaRange.searchRange(nums, 2)));
-
-	}
 
 }
