984d86e646e1e5499408df4867f72086fa6e876f
luckylau <laujunbupt0203@163.com>
2018-05-25 15:38:57 +0800

    二叉树插入数据

--- src/n/series/BinaryTreeAndDivideConquerAndDFSAndBFS/InsertaNodeinBinary.java
+++ src/n/series/BinaryTreeAndDivideConquerAndDFSAndBFS/InsertaNodeinBinary.java
@@ -1,74 +1,79 @@
+package n.series.BinaryTreeAndDivideConquerAndDFSAndBFS;
+
 /**
- * Given a binary search tree and a new tree node, insert the node into the tree. 
+ * Given a binary search tree and a new tree node, insert the node into the tree.
  * You should keep the tree still be a valid binary search tree.
  * Example
  * Given binary search tree as follow, after Insert node 6, the tree should be:
- *  2             2
+ * 2             2
  * / \           / \
- *1   4   -->   1   4
- *   /             / \ 
- *  3             3   6
- * 
+ * 1   4   -->   1   4
+ * /             / \
+ * 3             3   6
  */
-
-package n.series.BinaryTreeAndDivideConquerAndDFSAndBFS;
-
 public class InsertaNodeinBinary {
-	
-	public TreeNode insertNode(TreeNode root, TreeNode node) {
-        // write your code here
-        if( root == null ) {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(6);
+        root.left = node1;
+        root.right = node2;
+        node2.left = node3;
+        BinaryTreeLevelOrderTraversal binaryTreeLevelOrderTraversal = new BinaryTreeLevelOrderTraversal();
+        System.out.println(binaryTreeLevelOrderTraversal.levelOrder(root).toString());
+        InsertaNodeinBinary insertaNodeinBinary = new InsertaNodeinBinary();
+        System.out.println(binaryTreeLevelOrderTraversal.levelOrder(insertaNodeinBinary.insertNode(root, node4)));
+        TreeNode root2 = new TreeNode(2);
+        TreeNode node11 = new TreeNode(1);
+        TreeNode node21= new TreeNode(4);
+        TreeNode node31= new TreeNode(3);
+        TreeNode node41= new TreeNode(6);
+        root2.left = node11;
+        root2.right = node21;
+        node21.left = node31;
+        System.out.println(binaryTreeLevelOrderTraversal.levelOrder(insertaNodeinBinary.insertNode2(root2, node41)));
+
+    }
+
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        if (root == null) {
             return node;
         }
-        if(root.val > node.val){
-            root.left = insertNode(root.left,node);
-        }else {
-            root.right = insertNode(root.right,node);
+        if (root.val > node.val) {
+            root.left = insertNode(root.left, node);
+        } else {
+            root.right = insertNode(root.right, node);
         }
         return root;
     }
-	
-	public TreeNode insertNode2(TreeNode root, TreeNode node) {
-        // write your code here
-        if(root == null){
+
+    public TreeNode insertNode2(TreeNode root, TreeNode node) {
+        if (root == null) {
             root = node;
             return root;
         }
+
         TreeNode tmp = root;
         TreeNode last = null;
-        while(tmp != null){
+        while (tmp != null) {
             last = tmp;
-            if(tmp.val > node.val){
+            if (tmp.val > node.val) {
                 tmp = tmp.left;
-            }else{
+            } else {
                 tmp = tmp.right;
             }
         }
-        
-        if(last.val > node.val){
+
+        if (last.val > node.val) {
             last.left = node;
-        }else {
-            last.right =node;
+        } else {
+            last.right = node;
         }
         return root;
-        
+
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TreeNode root = new TreeNode(2);
-		TreeNode node1 = new TreeNode(1);
-		TreeNode node2 = new TreeNode(4);
-		TreeNode node3 = new TreeNode(3);
-		TreeNode node4 = new TreeNode(6);
-		root.left = node1;
-		root.right = node2;
-		node2.left = node3;
-		BinaryTreeLevelOrderTraversal binaryTreeLevelOrderTraversal = new BinaryTreeLevelOrderTraversal();
-		System.out.println(binaryTreeLevelOrderTraversal.levelOrder(root).toString());
-		InsertaNodeinBinary insertaNodeinBinary = new InsertaNodeinBinary();
-		System.out.println(binaryTreeLevelOrderTraversal.levelOrder(insertaNodeinBinary.insertNode(root, node4)));
-
-	}
 
 }
