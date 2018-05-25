f600cc7489a2998493e66242fe1a7d2852d80713
luckylau <laujunbupt0203@163.com>
2018-05-25 11:40:21 +0800

    对移位的有序数组二分查找

--- src/n/series/BinarySearchandSortedSearch/SearchinRotatedSortedArray.java
+++ src/n/series/BinarySearchandSortedSearch/SearchinRotatedSortedArray.java
@@ -1,3 +1,5 @@
+package n.series.BinarySearchandSortedSearch;
+
 /**
  * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
  * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
@@ -7,52 +9,50 @@
  * For [4, 5, 1, 2, 3] and target=1, return 2.
  * For [4, 5, 1, 2, 3] and target=0, return -1.
  */
-package n.series.BinarySearchandSortedSearch;
-
 public class SearchinRotatedSortedArray {
-	
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{6, 8, 15, 1, 2, 3, 4};
+        SearchinRotatedSortedArray searchinRotatedSortedArray = new SearchinRotatedSortedArray();
+        System.out.println(searchinRotatedSortedArray.search(nums, 8));
+
+    }
+
     public int search(int[] A, int target) {
-        // write your code here
-		if(A.length == 0){
-			return -1;
-		}
-		int start = 0;
-		int end = A.length -1;
-		
-		if(A[start] == target){
-		    return start;
-		}
-		if(A[end] == target){
-		    return end;
-		}
-		while (start+1 < end){
-			int mid = start + (end - start)/2;
-			if (A[mid] == target){
-				return mid;
-			}
-			if (A[mid] > A[start]){
-				if(A[mid] >= target && A[start] <= target){
-					end=mid;
-				}else{
-					start=mid;
-				}
-			}else if (A[mid] < A[start]){
-				if(A[mid] <= target && A[end] >= target){
-					start=mid;
-				}else{
-					end=mid;
-				}				
-			}
-		}
-		return -1;
-	}
+        if (A == null || A.length == 0) {
+            return -1;
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] nums = new int []{6,8,15,1,2,3,4};
-		SearchinRotatedSortedArray searchinRotatedSortedArray = new SearchinRotatedSortedArray();
-		System.out.println(searchinRotatedSortedArray.search(nums, 8));
+        int start = 0;
+        int end = A.length - 1;
 
-	}
+        if (A[start] == target) {
+            return start;
+        }
+        if (A[end] == target) {
+            return end;
+        }
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            }
+
+            if (A[mid] > A[start]) {
+                if (A[mid] >= target && A[start] <= target) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else if (A[mid] < A[start]) {
+                if (A[mid] <= target && A[end] >= target) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        return -1;
+    }
 
 }
