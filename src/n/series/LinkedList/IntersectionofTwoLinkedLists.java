bd992f21cf82ca8cd1a453d2d60c396778f8885d
luckylau <laujunbupt0203@163.com>
2018-05-30 11:41:54 +0800

    查找俩个链表的公共元素

--- src/n/series/LinkedList/IntersectionofTwoLinkedLists.java
+++ src/n/series/LinkedList/IntersectionofTwoLinkedLists.java
@@ -1,3 +1,4 @@
+package n.series.LinkedList;
 /**
  * Write a program to find the node at which the intersection of two singly linked lists begins.
  * Notice
@@ -6,83 +7,79 @@
  * You may assume there are no cycles anywhere in the entire linked structure.
  * Example
  * The following two linked lists:
-
- *A:          a1 �� a2
- *                �K
- *                    c1 �� c2 �� c3
- *                �J            
- *B:     b1 �� b2 �� b3
- * 
- * 
- * 
+ *   A:          a1 ->a2
+ *
+ *                        c1 ->c2 ->c3
+ *
+ *   B:     b1 ->b2 ->b3
+ *
+ *
+ *
  */
-package n.series.LinkedList;
-
 public class IntersectionofTwoLinkedLists {
-	
-	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // Write your code here
-        if(headA == null || headB ==null){
+
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        headA.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode headB = new ListNode(6);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(8);
+        ListNode node7 = new ListNode(5);
+        ListNode node8 = new ListNode(7);
+        headB.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node2;
+        System.out.println(ListNode.printListfromHeadtoTail(headA));
+        System.out.println(ListNode.printListfromHeadtoTail(headB));
+        IntersectionofTwoLinkedLists intersectionofTwoLinkedLists = new IntersectionofTwoLinkedLists();
+        System.out.println(intersectionofTwoLinkedLists.getIntersectionNode(headA, headB).val);
+
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
             return null;
         }
-        
+
         ListNode tail = headA;
-        while(tail.next !=null){
+        while (tail.next != null) {
             tail = tail.next;
         }
         tail.next = headB;
-        ListNode target =detectCycle(headA);
+        ListNode target = detectCycle(headA);
         tail.next = null;
         return target;
     }
-    private ListNode detectCycle(ListNode head){
-        if(head == null) {
+
+    private ListNode detectCycle(ListNode head) {
+        if (head == null) {
             return null;
         }
         ListNode slow = head;
         ListNode fast = head.next;
-        while(slow !=fast){
-            if(fast == null || fast.next == null) {
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
                 return null;
             }
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        while ( head != slow.next){
+
+        while (head != slow.next) {
             head = head.next;
             slow = slow.next;
         }
         return head;
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ListNode headA = new ListNode(1);
-		ListNode node = new ListNode(2);
-		ListNode node2 = new ListNode(3);
-		ListNode node3 = new ListNode(4);
-		ListNode node4 = new ListNode(5);
-		headA.next = node;
-		node.next = node2;
-		node2.next = node3;
-		node3.next = node4;
-		ListNode headB = new ListNode(6);
-		ListNode node5 = new ListNode(4);
-		ListNode node6 = new ListNode(8);
-		ListNode node7= new ListNode(5);
-		ListNode node8 = new ListNode(7);
-		headB.next = node5;
-		node5.next = node6;
-		node6.next =node7;
-		node7.next =node8;
-		node8.next = node2;
-		System.out.println(ListNode.printListfromHeadtoTail(headA));
-		System.out.println(ListNode.printListfromHeadtoTail(headB));
-		IntersectionofTwoLinkedLists intersectionofTwoLinkedLists = new IntersectionofTwoLinkedLists();
-		System.out.println(intersectionofTwoLinkedLists.getIntersectionNode(headA, headB).val);
-		
-
-	}
 
 }
