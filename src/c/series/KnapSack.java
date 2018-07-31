f59e9d498ea9dd7536d9e2c24fc6ee0f915be876
luckylau <laujunbupt0203@163.com>
2018-07-31 23:04:21 +0800

    修复错误

--- src/c/series/KnapSack.java
+++ src/c/series/KnapSack.java
@@ -46,3 +46,29 @@ public class KnapSack {
 	}
 
 }
+class Stuff{
+	private Integer value;
+
+	private Integer weight;
+
+	public Stuff(Integer value, Integer weight) {
+		this.value = value;
+		this.weight = weight;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+}
