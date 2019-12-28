b785092f86257509a39fedf61cce2689898df67a
luckylau <laujunbupt0203@163.com>
2019-12-28 23:43:01 +0800

    输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。

---
+++ src/util/TreeNode.java
@@ -0,0 +1,18 @@
+package util;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return this.val + "";
+    }
+
+}
---
+++ src/util/TreePrint.java
@@ -0,0 +1,18 @@
+package util;
+
+import n.series.BinaryTreeAndDivideConquerAndDFSAndBFS.BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+
+/**
+ * @Author luckylau
+ * @Date 2019/12/28
+ */
+public class TreePrint {
+
+    public static ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        BinaryTreeLevelOrderTraversal binaryTreeLevelOrderTraversal = new BinaryTreeLevelOrderTraversal();
+        return binaryTreeLevelOrderTraversal.levelOrder(root);
+    }
+
+}
