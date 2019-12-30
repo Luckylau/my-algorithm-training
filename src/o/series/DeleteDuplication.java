2a0ed0314488ef8e73751cbf43fc7eab2a0531c5
luckylau <laujunbupt0203@163.com>
2019-12-30 22:13:58 +0800

    在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。

---
+++ src/o/series/DeleteDuplication.java
@@ -0,0 +1,13 @@
+package o.series;
+/**
+ * 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+ * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
+ */
+
+/**
+ * @Author luckylau
+ * @Date 2019/12/30
+ */
+public class DeleteDuplication {
+    //解答见：/Users/jasonlau/gitcode/my-algorithm-training/src/n/series/LinkedList/RemoveDuplicatesFromSortedListII.java
+}
