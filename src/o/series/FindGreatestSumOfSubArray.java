320ee0e75beaccf8b323a6bdad96c5e30c38a48a
luckylau <laujunbupt0203@163.com>
2020-01-04 17:44:37 +0800

    给一个数组，返回它的最大连续子序列的和

---
+++ src/o/series/FindGreatestSumOfSubArray.java
@@ -0,0 +1,11 @@
+package o.series;
+
+/**
+ * 给一个数组，返回它的最大连续子序列的和
+ *
+ * @Author luckylau
+ * @Date 2019/12/17
+ */
+public class FindGreatestSumOfSubArray {
+    //解答见：/Users/yrd/Documents/github/my-algorithm-training/src/c/series/MaxSum.java
+}
