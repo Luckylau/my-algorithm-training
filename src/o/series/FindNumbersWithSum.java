033181af63a08cfcb66474b8078712e36b1153be
luckylau <laujunbupt0203@163.com>
2020-01-06 22:55:48 +0800

    输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。

---
+++ src/o/series/FindNumbersWithSum.java
@@ -0,0 +1,14 @@
+package o.series;
+/**
+ * 输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
+ */
+
+/**
+ * @Author luckylau
+ * @Date 2019/12/24
+ */
+public class FindNumbersWithSum {
+
+    //解答见：/Users/yrd/Documents/github/my-algorithm-training/src/c/series/FindSum.java
+    // 两个数越远，相乘积越小
+}
