aeee43ae3296f3beceff4cb81f136db67d525183
luckylau <laujunbupt0203@163.com>
2020-01-08 21:10:45 +0800

    输入一棵二叉树，判断该二叉树是否是平衡二叉树

---
+++ src/o/series/IsBalancedSolution.java
@@ -0,0 +1,14 @@
+package o.series;
+/**
+ * 输入一棵二叉树，判断该二叉树是否是平衡二叉树
+ */
+
+/**
+ * 左右子树的高度相差不超过 1 的树为平衡二叉树
+ *
+ * @Author luckylau
+ * @Date 2019/12/18
+ */
+public class IsBalancedSolution {
+    //解答见：/Users/yrd/Documents/github/my-algorithm-training/src/n/series/BinaryTreeAndDivideConquerAndDFSAndBFS/BalancedBinaryTree.java
+}
