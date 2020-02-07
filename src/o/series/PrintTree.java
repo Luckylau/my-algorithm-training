b897d1fe4eb07dda1ad733137a01e84cf81fd339
luckylau <laujunbupt0203@163.com>
2020-02-07 20:38:29 +0800

    PrintTree

---
+++ src/o/series/PrintTree.java
@@ -0,0 +1,12 @@
+package o.series;
+/**
+ * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+ */
+
+/**
+ * @Author luckylau
+ * @Date 2019/12/23
+ */
+public class PrintTree {
+    //解答见：/Users/yrd/Documents/github/my-algorithm-training/src/n/series/BinaryTreeAndDivideConquerAndDFSAndBFS/BinaryTreeLevelOrderTraversal.java
+}
