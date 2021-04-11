97738ed46c3e0a8b55de4e1fd0a4be56707ffc7e
luckylau <laujunbupt0203@163.com>
2021-04-11 23:11:19 +0800

    三数之和

---
+++ src/m/series/str/ThreeSum.java
@@ -0,0 +1,49 @@
+package m.series.str;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author luckylau
+ * @Date 2021/4/11
+ * 三数之和
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = 0 - nums[i];
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    res.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+}
