4747584fa594ba482c09ce39c98287ed8a5e6fba
luckylau <laujunbupt0203@163.com>
2021-04-12 00:16:29 +0800

    K 个一组翻转链表

---
+++ src/m/series/str/ReverseKGroup.java
@@ -0,0 +1,67 @@
+package m.series.str;
+
+/**
+ * @Author luckylau
+ * @Date 2021/4/12
+ */
+public class ReverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode hair = new ListNode(-1);
+        hair.next = head;
+        ListNode pre = hair;
+        ListNode end = hair;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode next = end.next;
+            end.next = null;
+            ListNode start = pre.next;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = start;
+        }
+        return hair.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
