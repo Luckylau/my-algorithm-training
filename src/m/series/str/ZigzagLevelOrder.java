ef225aa61c47d491dc586206dccdcf8dd4b861a9
luckylau <laujunbupt0203@163.com>
2021-04-12 23:41:03 +0800

    二叉树的锯齿形层序遍历

---
+++ src/m/series/str/ZigzagLevelOrder.java
@@ -0,0 +1,57 @@
+package m.series.str;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 二叉树的锯齿形层序遍历
+ *
+ * @Author luckylau
+ * @Date 2021/4/12
+ */
+public class ZigzagLevelOrder {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            if (!s1.isEmpty()) {
+                List<Integer> tmp = new ArrayList<>();
+                while (!s1.isEmpty()) {
+                    TreeNode node = s1.pop();
+                    tmp.add(node.val);
+                    if (node.left != null) {
+                        s2.push(node.left);
+                    }
+                    if (node.right != null) {
+                        s2.push(node.right);
+                    }
+                }
+                res.add(tmp);
+            }
+            if (!s2.isEmpty()) {
+                List<Integer> tmp = new ArrayList<>();
+                while (!s2.isEmpty()) {
+                    TreeNode node = s2.pop();
+                    tmp.add(node.val);
+                    if (node.right != null) {
+                        s1.push(node.right);
+                    }
+                    if (node.left != null) {
+                        s1.push(node.left);
+                    }
+                }
+                res.add(tmp);
+            }
+
+        }
+        return res;
+    }
+}
