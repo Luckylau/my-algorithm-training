434ff6824e9f8b77a68aa8078e18ff350ba2bb64
luckylau <laujunbupt0203@163.com>
2021-04-14 00:30:06 +0800

    相交链表

---
+++ src/m/series/str/GetIntersectionNode.java
@@ -0,0 +1,23 @@
+package m.series.str;
+
+/**
+ * 相交链表
+ *
+ * @Author luckylau
+ * @Date 2021/4/14
+ */
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode head1 = headA;
+        ListNode head2 = headB;
+        while (head1 != head2) {
+            head1 = head1 == null ? headB : head1.next;
+            head2 = head2 == null ? headA : head2.next;
+        }
+        return head1;
+    }
+
+    class ListNode {
+        ListNode next;
+    }
+}
