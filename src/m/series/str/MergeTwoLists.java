518ede9bd54220b11a3fd57965a931ee85de3ce2
luckylau <laujunbupt0203@163.com>
2021-04-15 00:41:20 +0800

    合并两个有序链表

---
+++ src/m/series/str/MergeTwoLists.java
@@ -0,0 +1,50 @@
+package m.series.str;
+
+/**
+ * 合并两个有序链表
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode dump = new ListNode(-1);
+        ListNode pre = dump;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                dump.next = l2;
+                l2 = l2.next;
+            } else {
+                dump.next = l1;
+                l1 = l1.next;
+            }
+            dump = dump.next;
+        }
+        dump.next = l1 == null ? l2 : l1;
+        return pre.next;
+
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
