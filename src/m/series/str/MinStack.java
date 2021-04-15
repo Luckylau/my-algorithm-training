5732e3b75bd6444dc73e04753a871383c47646f2
luckylau <laujunbupt0203@163.com>
2021-04-15 22:41:39 +0800

    最小栈

---
+++ src/m/series/str/MinStack.java
@@ -0,0 +1,45 @@
+package m.series.str;
+
+import java.util.Stack;
+
+/**
+ * 最小栈
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (!minStack.isEmpty() && val <= minStack.peek()) {
+            minStack.pop();
+        }
+
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
