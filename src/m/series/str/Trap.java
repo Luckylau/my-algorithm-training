de438de2f54940924bd227902c8b91d053fe6216
luckylau <laujunbupt0203@163.com>
2021-04-15 21:46:41 +0800

    接雨水

---
+++ src/m/series/str/Trap.java
@@ -0,0 +1,39 @@
+package m.series.str;
+
+/**
+ * 接雨水
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class Trap {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int res = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (maxLeft < height[left]) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (maxRight < height[right]) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+
+    }
+}
