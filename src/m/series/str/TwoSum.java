32ea12b340ea7c103dc0d2ea5945e78335e419e3
luckylau <laujunbupt0203@163.com>
2021-04-15 23:42:27 +0800

    两数之和

---
+++ src/m/series/str/TwoSum.java
@@ -0,0 +1,29 @@
+package m.series.str;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int[] res = new int[]{-1, -1};
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!hash.containsKey(target - nums[i])) {
+                hash.put(nums[i], i);
+            } else {
+                res[0] = i;
+                res[1] = hash.get(target - nums[i]);
+            }
+        }
+        return res;
+    }
+}
