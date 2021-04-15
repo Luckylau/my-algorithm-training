853c72ab942284a807b3f0fa5e94b1fdbfd82478
luckylau <laujunbupt0203@163.com>
2021-04-15 23:26:22 +0800

    字符串相加

---
+++ src/m/series/str/AddStrings.java
@@ -0,0 +1,26 @@
+package m.series.str;
+
+/**
+ * 字符串相加
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        int l1 = num1.length() - 1;
+        int l2 = num2.length() - 1;
+        StringBuffer sb = new StringBuffer();
+        int carry = 0;
+        while (l1 >= 0 || l2 >= 0 || carry != 0) {
+            int x = l1 < 0 ? 0 : num1.charAt(l1) - '0';
+            int y = l2 < 0 ? 0 : num2.charAt(l2) - '0';
+            int result = x + y + carry;
+            carry = result / 10;
+            sb.append(result % 10);
+            l1--;
+            l2--;
+        }
+        return sb.reverse().toString();
+    }
+}
