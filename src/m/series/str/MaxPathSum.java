d9bd886ee659e931e76f358f4748d84636e78222
luckylau <laujunbupt0203@163.com>
2021-04-15 22:30:36 +0800

    二叉树中的最大路径和

---
+++ src/m/series/str/MaxPathSum.java
@@ -0,0 +1,34 @@
+package m.series.str;
+
+import util.TreeNode;
+
+/**
+ * 二叉树中的最大路径和
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class MaxPathSum {
+    private int sum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        getMax(root);
+        return sum;
+    }
+
+    private int getMax(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int maxleft = Math.max(getMax(node.left), 0);
+        int maxRight = Math.max(getMax(node.right), 0);
+
+        sum = Math.max(sum, node.val + maxRight + maxleft);
+
+        return node.val + Math.max(maxleft, maxRight);
+    }
+}
