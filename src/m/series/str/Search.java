d03f2b0a1ccb2201f1f1c00c8c7e220dbe38a73c
luckylau <laujunbupt0203@163.com>
2021-04-15 22:18:17 +0800

    搜索旋转排序数组

---
+++ src/m/series/str/Search.java
@@ -0,0 +1,44 @@
+package m.series.str;
+
+/**
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class Search {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[start]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+
+    }
+}
