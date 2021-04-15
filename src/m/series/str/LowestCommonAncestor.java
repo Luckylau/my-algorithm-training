09f78cecbe575c442a2d0b4145f7645d9f6d6b0c
luckylau <laujunbupt0203@163.com>
2021-04-15 22:48:02 +0800

    二叉树的最近公共祖先

---
+++ src/m/series/str/LowestCommonAncestor.java
@@ -0,0 +1,27 @@
+package m.series.str;
+
+import util.TreeNode;
+
+/**
+ * 二叉树的最近公共祖先
+ *
+ * @Author luckylau
+ * @Date 2021/4/15
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+
+    }
+}
