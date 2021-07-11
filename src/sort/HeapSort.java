67928bea92c53736e60531e7cbc337140ca9959f
luckylau <laujunbupt0203@163.com>
2021-07-11 23:47:36 +0800

    堆排序的bug

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -13,25 +13,25 @@ public class HeapSort {
     }
 
     public void heapSort(int[] nums) {
-        if (nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return;
         }
         int len = nums.length;
-        for (int i = len / 2; i >= 0; i--) {
+        for (int i = (len - 1) / 2; i >= 0; i--) {
             heapAdjust(nums, i, len);
         }
         for (int i = len - 1; i >= 1; i--) {
             int tmp = nums[0];
             nums[0] = nums[i];
             nums[i] = tmp;
-            heapAdjust(nums, 0, i - 1);
+            heapAdjust(nums, 0, i);
         }
     }
 
     private void heapAdjust(int[] nums, int index, int length) {
         int tmp = nums[index];
-        for (int i = 2 * index + 1; i < length - 1; i = 2 * i + 1) {
-            if (nums[i] < nums[i + 1]) {
+        for (int i = 2 * index + 1; i < length; i = 2 * i + 1) {
+            if (i + 1 < length && nums[i] < nums[i + 1]) {
                 i++;
             }
             if (tmp >= nums[i]) {
@@ -41,7 +41,6 @@ public class HeapSort {
             index = i;
         }
         nums[index] = tmp;
-
     }
 
 }
