002764bd0a5d54f09741eff6a94eb0f8965d25c4
luckylau <laujunbupt0203@163.com>
2022-03-13 23:12:22 +0800

    fix(sort): mergeSort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -2,103 +2,66 @@ package sort;
 
 import java.util.Arrays;
 
+/**
+ * @Author luckylau
+ * @Date 2022/3/13
+ */
 public class MergeSort {
     public static void main(String[] args) {
         int[] nums = {2, 1, 3, 4, 9, 6, 7, 8, 5, 10, 5, 6};
         System.out.println(Arrays.toString(nums));
-        MergeSort MergeSort = new MergeSort();
-        MergeSort.mergeSort(nums);
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sortArray(nums);
         System.out.println(Arrays.toString(nums));
-        int[] nums2 = {2, 1, 3, 4, 9, 6, 7, 8, 5, 10, 5, 6};
-        System.out.println(Arrays.toString(nums2));
-        MergeSort.mergeSort2(nums2);
-        System.out.println(Arrays.toString(nums2));
     }
 
-    // �ݹ�
-    public void mergeSort(int[] nums) {
-        if (nums.length == 0) {
-            return;
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return nums;
         }
-        int len = nums.length;
-        helper(nums, 0, len - 1);
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
     }
 
-    private void helper(int[] nums, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            helper(nums, left, mid);
-            helper(nums, mid + 1, right);
-            merge(nums, left, right);
+
+    private void mergeSort(int[] nums, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start) / 2;
+            mergeSort(nums, start, mid);
+            mergeSort(nums, mid + 1, end);
+            merge(nums, start, end);
         }
     }
 
-    private void merge(int[] nums, int left, int right) {
-        int mid = left + (right - left) / 2;
-        int i = left;
-        int j = mid + 1;
+    private void merge(int[] nums, int start, int end) {
+        int mid = start + (end - start) / 2;
+        int left = start;
+        int right = mid + 1;
+        int[] tmp = new int[end - start + 1];
         int count = 0;
-        int[] tmp = new int[right - left + 1];
-        while (i <= mid && j <= right) {
-            if (nums[i] < nums[j]) {
-                tmp[count++] = nums[i++];
+        while (left <= mid && right <= end) {
+            if (nums[left] < nums[right]) {
+                tmp[count++] = nums[left++];
             } else {
-                tmp[count++] = nums[j++];
+                tmp[count++] = nums[right++];
             }
         }
-        while (i <= mid) {
-            tmp[count++] = nums[i++];
+        if (left <= mid) {
+            while (left <= mid) {
+                tmp[count++] = nums[left++];
+            }
         }
-        while (j <= right) {
-            tmp[count++] = nums[j++];
+
+        if (right <= end) {
+            while (right <= end) {
+                tmp[count++] = nums[right++];
+            }
         }
         count = 0;
-        while (left <= right) {
-            nums[left++] = tmp[count++];
+        while (start <= end) {
+            nums[start++] = tmp[count++];
         }
     }
 
-    // �ǵݹ�
-    public void mergeSort2(int[] nums) {
-        if (nums.length == 0) {
-            return;
-        }
-        int sublen = 1;
-        int length = nums.length;
-        while (sublen < length) {
-            for (int i = 0; i < length; i += 2 * sublen) {
-                merge2(nums, i, sublen);
-            }
-            sublen *= 2;
-        }
-    }
-
-    private void merge2(int[] nums, int i, int sublen) {
-        int start = i;
-        int count = 0;
-        int[] tmp = new int[2 * sublen];
-
-        int frontlen = nums.length > (i + sublen) ? (i + sublen) : nums.length;
-        int j = i + sublen;
-        int rearlen = nums.length > (j + sublen) ? (j + sublen) : nums.length;
-        while (i < frontlen && j < rearlen) {
-            if (nums[i] < nums[j]) {
-                tmp[count++] = nums[i++];
-            } else {
-                tmp[count++] = nums[j++];
-            }
-        }
-        while (i < frontlen) {
-            tmp[count++] = nums[i++];
-        }
-        while (j < rearlen) {
-            tmp[count++] = nums[j++];
-        }
-
-        for (int k = 0; k < count; k++) {
-            nums[start++] = tmp[k];
-        }
-
-    }
 
 }
