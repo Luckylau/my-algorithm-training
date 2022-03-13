f8b8fe8baa080f2c8cd423f6c0fcb81d7f033594
luckylau <laujunbupt0203@163.com>
2022-03-13 23:00:33 +0800

    fix(sort): heapSort

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -2,6 +2,10 @@ package sort;
 
 import java.util.Arrays;
 
+/**
+ * @Author luckylau
+ * @Date 2022/3/13
+ */
 public class HeapSort {
 
     public static void main(String[] args) {
@@ -16,22 +20,22 @@ public class HeapSort {
         if (nums == null || nums.length == 0) {
             return;
         }
-        int len = nums.length;
-        for (int i = (len - 1) / 2; i >= 0; i--) {
-            heapAdjust(nums, i, len);
+        int end = nums.length - 1;
+        for (int i = end / 2; i >= 0; i--) {
+            heapAdjust(nums, i, end);
         }
-        for (int i = len - 1; i >= 1; i--) {
+        for (int i = end; i >= 1; i--) {
             int tmp = nums[0];
             nums[0] = nums[i];
             nums[i] = tmp;
-            heapAdjust(nums, 0, i);
+            heapAdjust(nums, 0, i - 1);
         }
     }
 
     private void heapAdjust(int[] nums, int index, int length) {
         int tmp = nums[index];
-        for (int i = 2 * index + 1; i < length; i = 2 * i + 1) {
-            if (i + 1 < length && nums[i] < nums[i + 1]) {
+        for (int i = 2 * index + 1; i <= length; i = 2 * i + 1) {
+            if (i + 1 <= length && nums[i] < nums[i + 1]) {
                 i++;
             }
             if (tmp >= nums[i]) {
