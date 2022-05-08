d7505e7a1175364123cbe8b496d7ccf5045738ac
luckylau <laujunbupt0203@163.com>
2022-05-08 22:42:46 +0800

    feat(m.series): DiameterOfBinaryTree 543

---
+++ src/m/series/DiameterOfBinaryTree.java
@@ -0,0 +1,26 @@
+package m.series;
+
+import util.TreeNode;
+
+/**
+ * @Author luckylau
+ * @Date 2022/5/8
+ */
+public class DiameterOfBinaryTree {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return max;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = depth(root.left);
+        int rightDepth = depth(root.right);
+        max = Math.max(max, leftDepth + rightDepth);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
