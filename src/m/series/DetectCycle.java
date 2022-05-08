3d78ec1a7c7429fa93cfbc0422984a45f2e382c1
luckylau <laujunbupt0203@163.com>
2022-05-08 22:33:08 +0800

    feat(m.series): detectCycle 142

---
+++ src/m/series/DetectCycle.java
@@ -0,0 +1,31 @@
+package m.series;
+
+import util.ListNode;
+
+/**
+ * @Author luckylau
+ * @Date 2022/5/8
+ */
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = head;
+        fast = fast.next;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
